e5a0642950ef81188e8257b2a93c8c03062415cf
albg1996 <32883190+albg1996@users.noreply.github.com>
2017-11-21 13:50:29 -0800

    should work with new data

--- index.js
+++ index.js
@@ -16,26 +16,48 @@ var moviegroup=svg.append("g").attr("id","moviegroup");
 
 //DATA VARIABLE
 var csvdata=[];
-var characters=["character1","character2","character3","character4","character5","character6","character7","character8","character9","character10","character11","character12","character13",
-"character14","character15","character16","character17","character18","character19","character20","character21","character22","character23","character24","character25","character26",
-"character27","character28","character29","character30","character31","character32","character33","character34","character35"];
-var movies=["movie1","movie1","movie1","movie1","movie1","movie1","movie1","movie1","movie1","movie1","movie1","movie1","movie1","movie1","movie1","movie1"];
-var numberofcharacters=characters.length;
-var numberofmovies=movies.length;
+var character=[];
+var movies=[];
 
-
-d3.csv("marvel_data.csv",function(data){
+d3.csv("characters.csv",function(data){
        data.forEach(function(d){
-         //var tmp_char=JSON.parse(d.cast_crew);
-         var temp={
-			 id:d.id,
-			 title: d.title,
-			 characters:d.cast_crew
+         temp={
+			movie_name: d.moviename,
+			character_name: d.charactername,
+			time: d.screentime
 		 };
-		 csvdata.push(temp);
-    })
-
-
+		 
+		//make character and movie array
+		var ctemp=d.charactername;
+		var mtemp=d.moviename;
+		var charfound=false;
+		var moviefound=false;
+		for(var i=0;i<character.length && !charfound;i++){
+			if(ctemp==character[i]){
+				charfound=true;
+			}
+		}
+		//if character not found add to list
+		if(!charfound){
+			character.push(ctemp);
+		}
+		
+		for(var i=0;i<movies.length && !moviefound; i++){
+			if(mtemp==movies[i]){
+				moviefound=true;
+			}
+		}
+		//if movie not found add to list
+		if(!moviefound){
+			movies.push(mtemp);
+		}
+		//add the data to csv data
+		csvdata.push(temp);
+   })
+	//VARIABLES
+	var numberofcharacters =characters.length;
+	var numberofmovies=movies.length;
+	
 	//CREATE LABLES
 	var characterlabels = svg.selectAll("charactergroup")
 					.data(characters)
@@ -51,11 +73,11 @@ d3.csv("marvel_data.csv",function(data){
 					.enter()
 					.append("text")
 					.text(function(d,i){return d.title;})
-               .attr("transform", function (d,i) {
-                  var xText = ((width-180)/(numberofmovies+1)*(i+1)+50);
-                  var yText = header;
-                  return "translate(" + xText + "," + yText + ") rotate(-30)";
-               });
+					.attr("transform", function (d,i) {
+						var xText = ((width-180)/(numberofmovies+1)*(i+1)+50);
+						var yText = header;
+						return "translate(" + xText + "," + yText + ") rotate(-30)";
+					});
 
 	console.log(csvdata);
 })
